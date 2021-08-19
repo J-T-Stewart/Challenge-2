9018c4b301f7e94e762649cafabaf260cb5d941e
J-T-Stewart <justin.troy.stewart@gmail.com>
2021-08-19 14:36:32 -0700

    Transferring project to Github

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    private bool stateDog = true;
+    private float timeSpawnDog = 2;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (stateDog) {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                stateDog = false;
+                Invoke("allowDogSpawn", timeSpawnDog);
+            }
+        }
+    }
+
+    void allowDogSpawn() {
+        stateDog = true;
+    }
+}
